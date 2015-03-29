ed75e66860f9b687521627baf8da0b34f413a543
bexchauveto <olivier.bexchauvet@gmail.com>
2015-03-29 23:17:17 +0200

    Ajout lettre vueModifUserMail

--- vue/vueModifuserMail.php
+++ vue/vueModifuserMail.php
@@ -2,7 +2,7 @@
 	<fieldset>
 		<input type="hidden" value="2" name="typechange">
 		<input type="hidden" value="<?php echo $tuple['id']; ?>" name='idUser'/>
-		<p><label>Nouvelle adresse Emai</label> :
+		<p><label>Nouvelle adresse Email</label> :
 		<input type="email" id="newmail" name="newmail" required></p>
 		<input type="submit" value="modifier">
 	</fieldset>
