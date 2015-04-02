e03e1750b954bbda2abafdcdde967f78b5896c2b
bexchauveto <olivier.bexchauvet@gmail.com>
2015-04-02 02:10:20 +0200

    Correction Array

--- controleur/controlAjoutAnnonce.php
+++ controleur/controlAjoutAnnonce.php
@@ -5,7 +5,6 @@
 	$req = $mysqli->query("SELECT * FROM user WHERE pseudo = '$pseudo'") or die("ERROR");
 	$tuple = $req->fetch_array();
 	$reqVille = $mysqli->query("SELECT * FROM escale") or die("ERROR");
-	$tableauVille = new Array();
 	$i = 0;
 	while($tupleville = $reqVille->fetch_array()){
 		$tableauVille['$i'] = $tupleville['nomVille'];
