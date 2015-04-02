4cc2d24c5e0920572683f5a36f18546429d4dc6f
bexchauveto <olivier.bexchauvet@gmail.com>
2015-04-02 14:28:13 +0200

    Control inscription plus secure

--- controleur/controlInscription.php
+++ controleur/controlInscription.php
@@ -4,11 +4,17 @@
 	include("../modele/user.php");
 	$selsha1 = "coucou,ceciestleseldusha1lolololololololololookkk51";
 	$pseudo = $_POST['pseudo'];
-	$pwd = sha1($_POST['password'].$selsha1);
-	$mail = $_POST['email'];
-	$create = user::createUser($pseudo, $mail, $pwd);
-	if($create){
-		include("../vue/vueInscriptOK.php");
+	$req = $mysqli->query("SELECT * FROM user WHERE pseudo='$pseudo'");
+	if($req == false){
+		$pwd = sha1($_POST['password'].$selsha1);
+		$mail = $_POST['email'];
+		$create = user::createUser($pseudo, $mail, $pwd);
+		if($create){
+			include("../vue/vueInscriptOK.php");
+		}
+		else {
+			include("../vue/vueInscriptKO.php");
+		}
 	}
 	else {
 		include("../vue/vueInscriptKO.php");
