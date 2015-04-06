daac06c0d8027388e663510d0cbddeee9e1f3bcc
Kenairod <doriank@hotmail.fr>
2015-04-06 22:05:52 +0200

    Debut gestion du profil

---
+++ controleur/controlContruction.php
@@ -0,0 +1,12 @@
+<?php
+	include("./header.php");
+	$_SESSION['page'] = "";
+	include("./menu.php");
+	if(isset($_SESSION['user'])){
+		include("../vue/vueConstruction.php");
+	}
+	else {
+		header("Location:./controlIndex.php");
+	}
+	include("./footer.php");
+?>
---
+++ vue/vueConstruction.php
@@ -0,0 +1,4 @@
+<h1>Cette fonctionnalité n'existe pas encore !</h1>
+<p>Comme vous le savez le site est encore en cours d'achèvement, nous vous tiendrons au courant quand de nouvelles
+	 possiblités vous seront offertes. Merci d'utiliser Covoit'INSA ! En espérant que ce site vous aide dans vos recherches
+	 de covoiturage.</p>
