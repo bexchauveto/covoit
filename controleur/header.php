23f6251547830522fe55f32a242e819a70c388ef
Kenairod <doriank@hotmail.fr>
2015-04-08 23:39:31 +0200

    Ajout d'un favicon

--- controleur/header.php
+++ controleur/header.php
@@ -8,7 +8,7 @@
 		<title>Covoit'INSA</title>
 		<meta charset="utf-8" />
 		<link rel="stylesheet" href="../style.css" />
-		<link rel="icon" type="image/png" href="../Images/favicon.png" />
+		<link rel="icon" href="../images/favicon.ico" />
 		<script src="../js/jquery-1.11.2.min.js"></script>
 		<script src="../js/jquery-ui.min.js"></script>
 		<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha1.js"></script>
