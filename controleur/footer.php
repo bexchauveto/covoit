a0bd80fb2bbcf74f623644239e6b2d7bdc1dabb1
bexchauveto <olivier.bexchauvet@gmail.com>
2015-05-02 12:43:15 +0200

    Lien en noir non souligné pour le contact

--- controleur/footer.php
+++ controleur/footer.php
@@ -1,7 +1,7 @@
 </section>
 <footer>
 	<p> &copy; Amicale des Élèves INSA Toulouse</p>
-	<p>Nous contacter : <a href="mailto:covoit.insa.toulouse@gmail.com">covoit.insa.toulouse@gmail.com</a></p>
+	<p>Nous contacter : <a href="mailto:covoit.insa.toulouse@gmail.com" id='contact'>covoit.insa.toulouse@gmail.com</a></p>
 </footer>
 </div>
 </body>
