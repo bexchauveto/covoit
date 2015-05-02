ad5661ac0bba5b9fe63f34e469f7d1fffbf0cacf
Kenairod <doriank@hotmail.fr>
2015-05-02 14:13:44 +0200

    Correction injection SQL

--- controleur/controlConnexion.php
+++ controleur/controlConnexion.php
@@ -4,7 +4,9 @@
 	include("../modele/user.php");
 	$selsha1 = "coucou,ceciestleseldusha1lolololololololololookkk51";
 	$pseudo = htmlspecialchars($_POST['pseudo']);
+	$pseudo  = $mysqli->real_escape_string(get_magic_quotes_gpc() ? stripslashes($pseudo) : $pseudo);
 	$password = sha1($_POST['password'].$selsha1);
+	$password  = $mysqli->real_escape_string(get_magic_quotes_gpc() ? stripslashes($password) : $password);
 	$user = User::exist($pseudo, $password);
 	if ($user == True){
 		$_SESSION['user']=$pseudo;
